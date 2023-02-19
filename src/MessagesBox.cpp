e6900f9072807a164b1418669a00a75fcf3f4cb0
Maysam00Haj <maysam.hajyahia@outlook.com>
2023-02-19 07:17:12 +0200

    #90: added padding

--- src/MessagesBox.cpp
+++ src/MessagesBox.cpp
@@ -9,6 +9,9 @@ MessagesBox::MessagesBox(sf::Font* font) {
     this->rectangle.setFillColor(BOX_COLOR);
     this->rectangle.setOutlineColor(sf::Color::White);
     this->rectangle.setOutlineThickness(1.f);
+    this->frame.setPosition(10, 820);
+    this->frame.setSize({1000, 20});
+    this->frame.setFillColor(sf::Color(107, 107, 107, 255));
     this->messages =
     {
             "Click on the white circle to add a node"
@@ -18,9 +21,12 @@ MessagesBox::MessagesBox(sf::Font* font) {
     text->setFont(*font);
     text->setFillColor(sf::Color::White);
     text->setCharacterSize(20);
-    text->setPosition(20, 830);
+    text->setPosition(20, 850);
     this->texts.push_back(text);
     this->is_rendered.push_back(true);
+    this->minimized_rectangle.setPosition(10, 880);
+    this->minimized_rectangle.setSize({10, 100});
+    this->minimized_rectangle.setFillColor(BOX_COLOR);
 }
 
 
@@ -30,6 +36,9 @@ MessagesBox& MessagesBox::operator=(const MessagesBox& other) {
         this->messages = other.messages;
         this->texts = other.texts;
         this->is_rendered = other.is_rendered;
+        this->state = other.state;
+        this->minimized_rectangle = other.minimized_rectangle;
+        this->frame = other.frame;
     }
     return *this;
 }
@@ -39,13 +48,22 @@ MessagesBox::MessagesBox(MessagesBox &other) {
     this->messages = other.messages;
     this->texts = other.texts;
     this->is_rendered = other.is_rendered;
+    this->state = other.state;
+    this->minimized_rectangle = other.minimized_rectangle;
+    this->frame = other.frame;
 }
 
 
 void MessagesBox::render(sf::RenderTarget& target) {
-    target.draw(this->rectangle);
-    for (int i = 0; i < this->is_rendered.size(); i++)
-        if (is_rendered[i]) {
-            target.draw(*this->texts[0]);
-        }
+    if (this->state == MINIMIZED) {
+        target.draw(this->minimized_rectangle);
+    }
+    else if (this->state == REGULAR) {
+        target.draw(this->rectangle);
+        target.draw(this->frame);
+        for (int i = 0; i < this->is_rendered.size(); i++)
+            if (is_rendered[i]) {
+                target.draw(*this->texts[0]);
+            }
+    }
 }
--- src/MessagesBox.h
+++ src/MessagesBox.h
@@ -1,13 +1,24 @@
 #ifndef ALGOVIZ_MESSAGESBOX_H
 #define ALGOVIZ_MESSAGESBOX_H
 #include "SFML/Graphics.hpp"
+#include "Toolbar.h"
+
+enum BoxState {
+    REGULAR,
+    MINIMIZED
+};
+
 
 
 class MessagesBox {
 private:
+    BoxState state = REGULAR;
+    sf::RectangleShape minimized_rectangle;
     sf::RectangleShape rectangle;
+    sf::RectangleShape frame;
     std::vector<std::string> messages;
     std::vector<sf::Text*> texts;
+
 
 public:
     MessagesBox() = default;
